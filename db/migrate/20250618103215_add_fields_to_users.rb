685b142757699a84d934ff8565e6a4f0c55f760f
Amir Burokur <aamirbu23@gmail.com>
2025-06-19 19:40:23 +0400

    Fix migration duplicate bug

--- db/migrate/20250618103215_add_fields_to_users.rb
+++ db/migrate/20250618103215_add_fields_to_users.rb
@@ -1,9 +1,9 @@
 class AddFieldsToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :name, :string
-    add_column :users, :surname, :string
-    add_column :users, :dob, :date
-    add_column :users, :role, :string
-    add_column :users, :username, :string
+    add_column :users, :name, :string unless column_exists?(:users, :name)
+    add_column :users, :surname, :string unless column_exists?(:users, :surname)
+    add_column :users, :dob, :date unless column_exists?(:users, :dob)
+    add_column :users, :role, :string unless column_exists?(:users, :role)
+    add_column :users, :username, :string unless column_exists?(:users, :username)
   end
 end
