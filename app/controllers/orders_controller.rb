bb7d0acc139b00247435db731b3cfafc87d28d7e
Amir Burokur <aamirbu23@gmail.com>
2025-06-20 09:47:38 +0400

    Added payment page + alerts + small stock update in seeds.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+  def new
+  end
+
+  def create
+    flash[:notice] = "🎉 Payment successful! Thank you for your order."
+    redirect_to root_path
+  end
 end
