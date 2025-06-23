30f693fd3c6cadc8c7a2ad7b2bc50d0168b0cc2d
Amir Burokur <aamirbu23@gmail.com>
2025-06-23 11:39:33 +0400

    Added search function + MapBox

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -4,4 +4,7 @@ class Farmer < ApplicationRecord
   validates :name, presence: true
   validates :location, presence: true
   validates :contact_info, presence: true
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
