71f41f0f1dc586b600253cf6cccbcb668680cccf
Amir Burokur <aamirbu23@gmail.com>
2025-06-26 14:56:32 +0400

    Image Added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_24_122504) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_26_065600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_24_122504) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "image"
   end
 
   create_table "notifications", force: :cascade do |t|
