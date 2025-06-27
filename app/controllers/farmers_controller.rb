fc0d5a0d5f707e0df2406e9a25e0b158652672c1
tanvi gopaul <gopaultanvi05@gmail.com>
2025-06-27 12:33:34 +0400

    name + region + navbar

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -2,6 +2,8 @@ class FarmersController < ApplicationController
   def index
     if params[:search].present?
       @farmers = Farmer.where("location ILIKE :query OR name ILIKE :query OR region ILIKE :query", query: "%#{params[:search]}%")
+    elsif params[:region].present?
+      @farmers = Farmer.where("region ILIKE :query", query: "%#{params[:region]}%")
     else
       @farmers = Farmer.all
     end
